1965ff4ac132d88132787904f8e882ce9ad12de0
Opilane TTHK <opilane@tthk.ee>
2022-11-18 10:53:17 +0200

    Initial commit

---
+++ connect.php
@@ -0,0 +1,3 @@
+<?php
+$yhendus=new mysqli("localhost", "lastovskitarpv21", "123456798", "lastovskitarpv21");
+$yhendus->set_charset('utf8');
---
+++ kaubahaldus.php
@@ -0,0 +1,155 @@
+<?php
+  require("abifunktsioonid.php");
+    $sorttulp="nimetus";
+    $otsisona="";
+  if(isset($_REQUEST['sorttulp']))
+  {
+      $sorttulp=$_REQUEST['sorttulp'];
+  }
+  if(isSet($_REQUEST["grupilisamine"]) && !empty($_REQUEST['uuegrupinimi'])){
+    lisaGrupp($_REQUEST["uuegrupinimi"]);
+    header("Location: kaubahaldus.php");
+    exit();
+  }
+  if(isSet($_REQUEST["kaubalisamine"]) && !empty($_REQUEST['nimetus'])){
+    lisaKaup($_REQUEST["nimetus"], $_REQUEST["kaubagrupi_id"], $_REQUEST["hind"]);
+    header("Location: kaubahaldus.php");
+    exit();
+  }
+  if(isSet($_REQUEST["kustutusid"])){
+     kustutaKaup($_REQUEST["kustutusid"]);
+  }
+  if(isSet($_REQUEST["muutmine"])){
+     muudaKaup($_REQUEST["muudetudid"], $_REQUEST["nimetus"],
+                              $_REQUEST["kaubagrupi_id"], $_REQUEST["hind"]);
+  }
+  $kaubad=kysiKaupadeAndmed();
+?>
+<!DOCTYPE html>
+  <head>
+      <title>Kaupade leht</title>
+      <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  </head>
+  <div>
+   <form action="kaubahaldus.php">
+   <div id="kauba">
+     <h2>Kauba lisamine</h2>
+     <dl>
+       <dt>Nimetus:</dt>
+       <dd><input type="text" name="nimetus" /></dd>
+       <dt>Kaubagrupp:</dt>
+       <dd><?php
+         echo looRippMenyy("SELECT id, grupinimi FROM kaubagrupid", 
+                           "kaubagrupi_id");
+       ?>
+       </dd>
+       <dt>Hind:</dt>
+       <dd><input type="text" name="hind" /></dd>
+     </dl>
+     <input type="submit" name="kaubalisamine" value="Lisa kaup" />
+   </div>
+   <div id="grupi">
+     <h2>Grupi lisamine</h2>
+     <input type="text" name="uuegrupinimi" />
+     <input type="submit" name="grupilisamine" value="Lisa grupp" />
+   </div>
+   </form>
+   <form action="kaubahaldus.php">
+   <div id="loetelu">
+     <h2>Kaupade loetelu</h2>
+     <table>
+       <tr>
+         <th>Haldus</th>
+           <th><a href="kaubahaldus.php?sorttulp=nimetus">Nimetus</a></th>
+           <th><a href="kaubahaldus.php?sorttulp=grupinimi">Kaubagrupp</a></th>
+           <th><a href="kaubahaldus.php?sorttulp=hind">Hind</a></th>
+       </tr>
+       <?php foreach($kaubad as $kaup): ?>
+         <tr>
+          <?php if(isSet($_REQUEST["muutmisid"]) &&
+                            intval($_REQUEST["muutmisid"])==$kaup->id): ?>
+           <td>
+             <input type="submit" name="muutmine" value="Muuda" />
+             <input type="submit" name="katkestus" value="Katkesta" />
+             <input type="hidden" name="muudetudid" value="<?=$kaup->id ?>" />
+           </td>
+           <td><input type="text" name="nimetus" value="<?=$kaup->nimetus ?>" /></td>
+           <td><?php
+             echo looRippMenyy("SELECT id, grupinimi FROM kaubagrupid", 
+                                        "kaubagrupi_id", $kaup->kaubagrupi_id);
+           ?></td>
+           <td><input type="text" name="hind" value="<?=$kaup->hind ?>" /></td>
+          <?php else: ?>
+           <td><a class="tdx" href="kaubahaldus.php?kustutusid=<?=$kaup->id ?>"
+                  onclick="return confirm('Kas ikka soovid kustutada?')">x</a>
+               <a class="tdm" href="kaubahaldus.php?muutmisid=<?=$kaup->id ?>">m</a>
+           </td>
+           <td><?=$kaup->nimetus ?></td>
+           <td><?=$kaup->grupinimi ?></td>
+           <td><?=$kaup->hind ?></td>
+          <?php endif ?>
+         </tr>
+       <?php endforeach; ?>
+     </table>
+   </div>
+   </form>
+  </div>
+  </body>
+</html>
+<style>
+    table
+    {
+        border: 3pt solid black;
+    }
+    td
+    {
+        border: 2pt solid darkred;
+        font-size: 16pt;
+    }
+    #loetelu
+    {
+        box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.4);
+        width: 23%;
+        position: absolute;
+        width: 33%;
+    }
+    #loetelu > table
+    {
+        width: 100%;
+    }
+    #kauba {
+        position: absolute;
+        left: 60%;
+        border: 2pt solid black;
+        box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.4);
+        padding: 1%;
+    }
+    #grupi {
+        position: absolute;
+        left: 40%;
+        border: 2pt solid black;
+        box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.4);
+        padding: 1%;
+
+    }
+    #kauba > dl
+    {
+
+        font-size: 18px;
+        text-shadow: 1px 0px blue;
+        font-weight: bold;
+    }
+    .tdx
+    {
+        color: red;
+    }
+    .tdm
+    {
+        color: green;
+    }
+    .tdx:hover, .tdm:hover
+    {
+        transition: 0.25s;
+        text-shadow: 2px 0 black;
+    }
+</style>
